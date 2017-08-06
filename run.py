2a17141add5ff04543b347749506a96c32e4ad0c
romanyakovlev <yakovlevroman8@gmail.com>
2017-08-07 02:26:42 +0700

    add bootstrap

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ def render_static_to_html(template_environment, root_folder_path):
   static_template = template_environment.get_template(relative_path_static)
   #path_to_dir = os.path.join(os.sep, '19_site_generator)
   #static_template.stream(path_to_dir= os.getcwd()+'/').dump(path_to_static)
-  static_template.stream(path_to_dir= '/').dump(path_to_static)
+  static_template.stream(path_to_dir= '').dump(path_to_static)
 
 
 def get_json_config():
